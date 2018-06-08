d6b149b9331caad1899f9b36a928786af4b55f9b
Jim Palowski <palowskijim@gmail.com>
2018-06-08 12:26:28 -0700

    Add Input and Output for posting

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Post } from '../models/post.model';
+
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class PostsComponent  {
+  posts: Post[] = [
+    new Post('New Post', 'ANgry Joe')
+  ];
+  @Input() childPost: Post[];
+  @Output() clickSender = new EventEmitter();
 }
