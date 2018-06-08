d090e1fa8025b61f17143d6e803fd2d981e10ce7
Jim Palowski <palowskijim@gmail.com>
2018-06-08 11:09:47 -0700

    Added new Post for AppComponent also importing the model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Post } from './models/post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  currentPost: string = 'Angry Show'
+  posting: Post[] = [
+    new Post('Blog', 'Checkout my new angry review'),
+  ];
 }
