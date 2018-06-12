c441105c9a6e4cbbb85c403d38d309e271007d7f
Jim Palowski <palowskijim@gmail.com>
2018-06-12 16:18:56 -0700

    Add Posts service to connect to firebase along with making the post model

---
+++ src/app/posts.model.ts
@@ -0,0 +1,5 @@
+export class Posts{
+    constructor(public title:string, public author: string,  
+        public description: string) { }
+}
+//working on image
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Posts } from './posts.model';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class PostsService {
+  posts: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private database: AngularFireDatabase) { 
+    this.posts = database.list('posts');
+  }
 }
